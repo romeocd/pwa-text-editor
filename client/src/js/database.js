f80c80f1d2736e5cabb42441e4b20220b36a2c7a
romeo <rdumlao07@gmail.com>
2024-01-13 14:59:57 -0500

    update putDb and get Db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,24 +13,32 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
-  const db = await openDB('jate', 1);
-  const tx = db.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate')
-  const request = store.put({ id: 1, content});
-  const result = await request;
-  console.log('Data saved to the database', result);
+export const putDb = async (content, id) => {
+  try {
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.put({ id, content });
+    const result = await request;
+    console.log('Data saved to the database', result);
+  } catch (error) {
+    console.error('Could not save to the database', error);
+  }
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const db = await openDB('jate', 1);
-  const tx = db.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
-  const request = store.get(1);
-  const result = await request;
-  console.log('Data retrieved from database', result);
-  return result?.content;
+  try{
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = id ? store.get(id) : store.getAll();
+    const result = await request;
+    console.log('Data retrieved from database', result);
+    return id ? result?.content : result;
+  } catch (error) {
+    console.error('Could not get data from the database', error);
+  }
 };
 
 initdb();
